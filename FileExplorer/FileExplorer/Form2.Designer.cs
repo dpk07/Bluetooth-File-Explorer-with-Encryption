406303f9d24c071109f30be18541cf8853c2c862
Deepak Pai <paidpk07@gmail.com>
2018-02-01 19:46:01 +0530

    Added bluetooth Search

--- FileExplorer/FileExplorer/Form2.Designer.cs
+++ FileExplorer/FileExplorer/Form2.Designer.cs
@@ -28,48 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.regDevice = new System.Windows.Forms.Button();
+            this.unlockFolder = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.lockFolder = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // regDevice
             // 
-            this.button1.Location = new System.Drawing.Point(89, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(214, 75);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Register your device";
-            this.button1.UseVisualStyleBackColor = true;
+            this.regDevice.Location = new System.Drawing.Point(89, 54);
+            this.regDevice.Name = "regDevice";
+            this.regDevice.Size = new System.Drawing.Size(214, 75);
+            this.regDevice.TabIndex = 0;
+            this.regDevice.Text = "Register your device";
+            this.regDevice.UseVisualStyleBackColor = true;
+            this.regDevice.Click += new System.EventHandler(this.regDevice_Click);
             // 
-            // button3
+            // unlockFolder
             // 
-            this.button3.Location = new System.Drawing.Point(89, 156);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(214, 75);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Unlock and Decrypt folder";
-            this.button3.UseVisualStyleBackColor = true;
+            this.unlockFolder.Location = new System.Drawing.Point(89, 156);
+            this.unlockFolder.Name = "unlockFolder";
+            this.unlockFolder.Size = new System.Drawing.Size(214, 75);
+            this.unlockFolder.TabIndex = 2;
+            this.unlockFolder.Text = "Unlock and Decrypt folder";
+            this.unlockFolder.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // result
             // 
-            this.textBox1.Location = new System.Drawing.Point(58, 293);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(555, 81);
-            this.textBox1.TabIndex = 4;
+            this.result.Location = new System.Drawing.Point(58, 293);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(555, 81);
+            this.result.TabIndex = 4;
             // 
-            // button2
+            // lockFolder
             // 
-            this.button2.Location = new System.Drawing.Point(366, 54);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(214, 75);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Lock and Encrypt folder";
-            this.button2.UseVisualStyleBackColor = true;
+            this.lockFolder.Location = new System.Drawing.Point(366, 54);
+            this.lockFolder.Name = "lockFolder";
+            this.lockFolder.Size = new System.Drawing.Size(214, 75);
+            this.lockFolder.TabIndex = 5;
+            this.lockFolder.Text = "Lock and Encrypt folder";
+            this.lockFolder.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
@@ -89,19 +91,30 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Result:";
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.Visible = false;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(683, 386);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lockFolder);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.unlockFolder);
+            this.Controls.Add(this.regDevice);
             this.Name = "Form2";
             this.Text = "OptionsPage";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,11 +122,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button regDevice;
+        private System.Windows.Forms.Button unlockFolder;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button lockFolder;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
--- FileExplorer/FileExplorer/Form2.cs
+++ FileExplorer/FileExplorer/Form2.cs
@@ -7,14 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using InTheHand;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Ports;
+using InTheHand.Net.Sockets;
+using InTheHand.Windows.Forms;
+using System.IO;
+using System.Management;
 
 namespace FileExplorer
 {
+    
+
+
     public partial class Form2 : Form
-    {
+    {   
+
         public Form2()
         {
             InitializeComponent();
-        }
+            
+
     }
-}
+
+        private BluetoothAddress m_deviceAddr;
+        private String DeviceName;
+        private String DeviceAddress;
+        
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+    private void regDevice_Click(object sender, EventArgs e)
+        {
+         
+            SelectBluetoothDeviceDialog selDia;
+
+            selDia = new SelectBluetoothDeviceDialog();
+
+            if (selDia.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            m_deviceAddr = selDia.SelectedDevice.DeviceAddress;
+            //m_deviceTextbox.Text = selDia.SelectedDevice.DeviceName;
+            DeviceName = selDia.SelectedDevice.DeviceName;
+            DeviceAddress =
+                selDia.SelectedDevice.DeviceAddress.ToString();
+            result.AppendText(DeviceName + DeviceAddress);
+        }
+
+
+
+    }
+        
+
+
+    }
+
+
+

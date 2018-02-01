095b7a5f92d0b332c4ee6843f51e9ea687013ed8
Deepak Pai <paidpk07@gmail.com>
2018-02-01 19:50:58 +0530

    bt search added

--- FileExplorer/FileExplorer/Form2.cs
+++ FileExplorer/FileExplorer/Form2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,66 +9,98 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using InTheHand;
-using InTheHand.Net;
 using InTheHand.Net.Bluetooth;
 using InTheHand.Net.Ports;
 using InTheHand.Net.Sockets;
-using InTheHand.Windows.Forms;
 using System.IO;
 using System.Management;
 
 namespace FileExplorer
 {
-    
-
-
+       
     public partial class Form2 : Form
     {   
 
         public Form2()
         {
             InitializeComponent();
-            
-
-    }
-
-        private BluetoothAddress m_deviceAddr;
-        private String DeviceName;
-        private String DeviceAddress;
-        
+            bg = new BackgroundWorker();
+            bg.DoWork += new DoWorkEventHandler(bg_DoWork);
+            bg.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bg_RunWorkerCompleted);
+        }
 
         private void Form2_Load(object sender, EventArgs e)
         {
 
         }
+        BackgroundWorker bg;
         
-    private void regDevice_Click(object sender, EventArgs e)
+
+        void bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-         
-            SelectBluetoothDeviceDialog selDia;
-
-            selDia = new SelectBluetoothDeviceDialog();
-
-            if (selDia.ShowDialog() != DialogResult.OK)
-            {
-                return;
-            }
-
-            m_deviceAddr = selDia.SelectedDevice.DeviceAddress;
-            //m_deviceTextbox.Text = selDia.SelectedDevice.DeviceName;
-            DeviceName = selDia.SelectedDevice.DeviceName;
-            DeviceAddress =
-                selDia.SelectedDevice.DeviceAddress.ToString();
-            result.AppendText(DeviceName + DeviceAddress);
+            Console.Write(e.Result);
         }
 
+        void bg_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<Device> devices = new List<Device>();
+            InTheHand.Net.Sockets.BluetoothClient bc = new InTheHand.Net.Sockets.BluetoothClient();
+            InTheHand.Net.Sockets.BluetoothDeviceInfo[] array = bc.DiscoverDevices();
+            int count = array.Length;
+            for (int i = 0; i < count; i++)
+            {
+                Device device = new Device(array[i]);
+                devices.Add(device);
+            }
+            e.Result = devices;
+        }
 
+        private void btn_find_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        
 
-    }
+        private void regDevice_Click(object sender, EventArgs e)
+        {
+            if (!bg.IsBusy)
+            {
+                bg.RunWorkerAsync();
+            }
+        }
+        //Guid mUUID = new Guid("00001101-0000-1000-8000-00805F9B34FB");
         
 
 
     }
+    public class Device
+    {
+        public string DeviceName { get; set; }
+        public bool Authenticated { get; set; }
+        public bool Connected { get; set; }
+        public ushort Nap { get; set; }
+        public uint Sap { get; set; }
+        public DateTime LastSeen { get; set; }
+        public DateTime LastUsed { get; set; }
+        public bool Remembered { get; set; }
+
+        public Device(BluetoothDeviceInfo device_info)
+        {
+            this.Authenticated = device_info.Authenticated;
+            this.Connected = device_info.Connected;
+            this.DeviceName = device_info.DeviceName;
+            this.LastSeen = device_info.LastSeen;
+            this.LastUsed = device_info.LastUsed;
+            this.Nap = device_info.DeviceAddress.Nap;
+            this.Sap = device_info.DeviceAddress.Sap;
+            this.Remembered = device_info.Remembered;
+        }
+
+        public override string ToString()
+        {
+            return this.DeviceName;
+        }
+    }
 
 
-
+}

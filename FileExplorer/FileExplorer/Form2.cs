fa79d545a5f7efb1ea3b63158fa7ea1f074589b8
Deepak Pai <paidpk07@gmail.com>
2018-02-10 15:27:25 +0530

    Added database for login and logic for checking if device in range

--- FileExplorer/FileExplorer/Form2.cs
+++ FileExplorer/FileExplorer/Form2.cs
@@ -11,6 +11,7 @@ using System.Threading;
 using InTheHand;
 using InTheHand.Net;
 using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Bluetooth.Widcomm;
 using InTheHand.Net.Ports;
 using InTheHand.Net.Sockets;
 using System.IO;
@@ -34,31 +35,88 @@ namespace FileExplorer
         }
 
 
-        BluetoothAddress deviceAddr;
+        String deviceAddr;
+        BluetoothDeviceInfo di = null;
+        BluetoothClient bc = new BluetoothClient();
+        DeviceScanner ds;
+        Thread tr;
         String deviceName;
  private void regDevice_Click(object sender, EventArgs e)
         {
             SelectBluetoothDeviceDialog selDia;
 
             selDia = new SelectBluetoothDeviceDialog();
-            selDia.ShowUnknown = true;
-            selDia.AddNewDeviceWizard = true;
-            selDia.ShowDialog();
-            selDia.AddNewDeviceWizard = false;
+            //selDia.ShowUnknown = true;
+            //selDia.AddNewDeviceWizard = true;
+            //selDia.ShowDialog();
+            //selDia.AddNewDeviceWizard = false;
 
             if (selDia.ShowDialog() != DialogResult.OK)
             {
                 return;
             }
 
-            deviceAddr = selDia.SelectedDevice.DeviceAddress;
+            deviceAddr = (selDia.SelectedDevice.DeviceAddress).ToString();
             deviceName = selDia.SelectedDevice.DeviceName;
             result.AppendText(deviceAddr + " " + deviceName);
+            ds =new DeviceScanner(deviceAddr);
+            ds.Start();
 
         }
+        private void ServiceThreadWrapper()
+        {
+            try
+            {
+                ServiceThread();
+            }
+            catch (Exception e)
+            {
+                //ExceptionHandler(e);
+                Environment.Exit(-1);
+            }
+        }
 
+        private void ServiceThread()
+        {
+            var t = new System.Threading.Timer(o => check(), null, 0, 10000);
+            
 
+        }
+        
+        
+        public void check()
+        {
+            if (ds.DeviceInRange)
+                setText("Device is in range ");
+            else
+                setText("Device is not in range ");
 
+        }
+        public void setText(String s)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(setText), new object[] { s });
+                return;
+            }
+            result.AppendText(s);
+
+        }
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            
+            
+            tr = new Thread(ServiceThreadWrapper);
+            tr.Start();
+
+            //ds.Stop();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {   
+            System.Environment.Exit(1);
+
+        }
     }
     
 
